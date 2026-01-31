aacea7797458941c681c9398866dab4c993be81a
jkmloom <jatinmehtagaya@gmail.com>
2026-01-31 13:26:04 +0530

    update 01.2-array.py

--- 01.2-array.py
+++ 01.2-array.py
@@ -3,7 +3,13 @@ from array import *
 # creating an array (on user input)
 arr = array('i', [])
 
+print("")
 n = int(input("How many elements do you want in this array? "))
 
-for i in range(len(arr)):
-    pass
+for i in range(1, n+1):
+    arr.append(int(input("Enter next element: ")))
+
+print(f"{"***"*10}\nFinal Array: ")
+for x in arr:
+    print(x, end=' ')
+print()
